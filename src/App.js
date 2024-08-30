82327ede948e5ee06d445d5b04e00d417a69bc08
Anis-CS <anisurrhaman0133@gmail.com>
2024-08-30 22:14:27 +0600

    nested data maping

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+const users=[
+  {
+    FullName:"Anisur Rahman",
+    Address:"Dhaka",
+    Phones:[
+      {Home:"249249",Office:"53573953"}
+    ]
+  },
+  {
+    FullName:"Shahin Mia",
+    Address:"Mirpur",
+    Phones:[
+      {Home:"24920",Office:"5357675"}
+    ]
+  },
+  {
+    FullName:"Kamal Rahman",
+    Address:"Farmgate",
+    Phones:[
+      {Home:"24922232",Office:"53573934"}
+    ]
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {users.map((user,index) =>
+        <article key={index}>
+            <h2>Name :{user.FullName}</h2>
+            <h2>Address :{user.Address}</h2>
+            <p>
+              Phones : {user.Phones.map((phone,index) => 
+              <div key={index}>
+                <p>home : {phone.Home}</p>
+                <p>Office : {phone.Office}</p>
+               </div>
+              )}
+            </p>
+        </article>
+        
+      )}
     </div>
   );
 }
