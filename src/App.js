7c4256b5c0bf23fffa15fe6a98b351284b6c5f61
Anis-CS <anisurrhaman0133@gmail.com>
2024-08-31 19:30:35 +0600

    try all program

--- src/App.js
+++ src/App.js
@@ -3,46 +3,37 @@ import './App.css';
 
 const users=[
   {
-    FullName:"Anisur Rahman",
-    Address:"Dhaka",
-    Phones:[
-      {Home:"249249",Office:"53573953"}
-    ]
+    Name:'Anis',
+    Address:'Dhaka',
+    Phones :[ {Home:'393934234', Office:'7997885'} ]
   },
   {
-    FullName:"Shahin Mia",
-    Address:"Mirpur",
-    Phones:[
-      {Home:"24920",Office:"5357675"}
-    ]
+    Name:'Anis',
+    Address:'Dhaka',
+    Phones:[ { Home:'39393544234', Office:'7993437885'}]
   },
   {
-    FullName:"Kamal Rahman",
-    Address:"Farmgate",
-    Phones:[
-      {Home:"24922232",Office:"53573934"}
-    ]
+    Name:'Anis',
+    Address:'Dhaka',
+    Phones :[ { Home:'393934334234',Office:'79973434885'}]
   }
-];
+]
 
 function App() {
   return (
     <div className="App">
         {users.map((user,index) =>
-        <article key={index}>
-            <h2>Name :{user.FullName}</h2>
-            <h2>Address :{user.Address}</h2>
-            <p>
-              Phones : {user.Phones.map((phone,index) => 
-              <div key={index}>
-                <p>home : {phone.Home}</p>
-                <p>Office : {phone.Office}</p>
-               </div>
+          <article key={index}>
+            <h1>Name : {user.Name}</h1>
+            <h2>Address : {user.Address}</h2>
+            <p>Phones : {user.Phones.map((phone,index) => 
+              <p key={index}>
+                   Home : {phone.Home} Office : {phone.Office}
+              </p>
               )}
             </p>
         </article>
-        
-      )}
+        )}
     </div>
   );
 }
